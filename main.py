49ed6c7c2182ed4ab82bcb3e0f085e5c4da22598
Nayma-konok <naymakonok002@gmail.com>
2025-11-16 20:39:27 +0000

    Hotel booking system with OOP

---
+++ main.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+df=pd.read_csv("hotels.csv", dtype={"id":str})
+
+
+class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id=hotel_id
+        self.name=df.loc[df["id"]==self.hotel_id, "name"].squeeze()
+       
+
+    def book_hotel(self):
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"]==self.hotel_id, "available"]= "no"
+        df.to_csv("hotels.csv", index=False)
+        
+
+    def available(self):
+        """Check if the hotel is available"""
+        availability=df.loc[df["id"]==self.hotel_id, "available"].squeeze()
+        if availability =="yes":
+            return True
+        else:
+            return False
+
+
+class ReservationConfermation:
+    def __init__(self, customer_name, hotel_obj):
+        self.customer_name=customer_name
+        self.hotel=hotel_obj
+
+    def generate(self):
+        content=f"""
+        Thank you for yout reservation
+        Here is your booking data:
+        Name:{self.customer_name}
+        Hotel name:{self.hotel.name}
+        """
+        return content
+
+print(df)
+
+hotel_ID=input("Enter the id of the hotel:")
+hotel=Hotel(hotel_ID)
+
+if hotel.available():
+    hotel.book_hotel()
+    name=input("Enter a name:")
+    reservation_confermation=ReservationConfermation(customer_name=name, hotel_obj=hotel)
+    print(reservation_confermation.generate())
+
+else:
+    print("Hotel is not free")
